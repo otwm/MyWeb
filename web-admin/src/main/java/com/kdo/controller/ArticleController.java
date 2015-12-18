63066bc477f36679390b43928cffbf3599af4bf1
kdo <otwmoo@gmail.com>
2015-12-19 01:43:21 +0900

    566ef60df0bf16854c346378 게시판 만들기

--- web-admin/src/main/java/com/kdo/controller/ArticleController.java
+++ web-admin/src/main/java/com/kdo/controller/ArticleController.java
@@ -1,8 +1,13 @@
 package com.kdo.controller;
 
+import com.kdo.article.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
 
 /**
  * Created by kdo on 2015-12-15.
@@ -10,8 +15,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/article")
 public class ArticleController {
+    @Resource
+    private ArticleRepository articleRepository;
+
     @RequestMapping(method = RequestMethod.GET)
-    String list() {
+    String find(Model model) {
+        model.addAttribute("articles", articleRepository.findAll());
         return "article/articleList";
     }
 }
